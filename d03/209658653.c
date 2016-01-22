66c54a4f259d7e23f6e6074145e945c5fd5ae1c8
Athziri Gutierrez <athzirimx@hotmail.com>
2016-01-21 19:35:20 -0600

    Agregue comentarios al archivo de Mauricio

--- d03/209658653.c
+++ d03/209658653.c
@@ -1,5 +1,6 @@
 //Comentario propio. Mauricio Gerardo Lazcano Origel. 209658653
 //Segundo Comentario. Noel Vela Larios 209483632
+//HOla Mauricio hola noel 209676988 soy AThziri
 
 #include <stdio.h>
 #include <stdlib.h>
