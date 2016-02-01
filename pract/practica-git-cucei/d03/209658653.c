3a9122a60d719bb63de2132ffa80911d090b23d6
José Adán Pérez Hau <Jos Adn Prez Hau>
2016-02-01 05:40:04 -0600

    Hace correciones

---
+++ pract/practica-git-cucei/d03/209658653.c
@@ -0,0 +1,12 @@
+//Comentario propio. Mauricio Gerardo Lazcano Origel. 209658653
+//Segundo Comentario. Noel Vela Larios 209483632
+//HOla Mauricio hola noel 209676988 soy AThziri
+//Hola este programa es popular lol 212355149 Daniel
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("Hola mundo!");
+	return 0;
+}
---
+++ pract/practica-git-cucei/d03/212354908.c
@@ -0,0 +1,10 @@
+//Primer comentario, Lopez Fonseca Lidia Paola
+
+#include<stdio.h>
+ 
+int main(void) {
+ 
+	printf("HOLA MUNDO");//No compila XD, Zarate Rodriguez Flor Edith
+ 
+	return 0;
+}
